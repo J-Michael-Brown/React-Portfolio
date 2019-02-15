e128e44d9b2fad4267d8c481c6e441a5ae8fc095
John Michael Brown <jmichaelbrown132737@gmail.com>
2019-02-15 13:04:55 -0800

    add example js object to build off of

---
+++ src/assets/portfolio.js
@@ -0,0 +1,23 @@
+const Portfolio = {
+  educationList: [
+    {
+      institution: 'Portland Community College',
+      skillsGained: [
+        'Studied Computer Science up to a 162 level',
+        'Finished Math up to a 253 level'
+      ]
+    },
+    {
+      institution: 'Epicodus, Ruby/React Track',
+      skillsGained: [
+        'Completed full-time, 27-week program in web and open-source development',
+        'Used command line and Git from day 1 of class',
+        'Studied raw SQL',
+        'Built over 60 applications with Angular, Ruby, React, jQuery, HTML, and CSS',
+        'Developed interpersonal and communication skills through pairing and group projects'
+      ]
+    }
+  ]
+};
+
+export default Portfolio;
