0f195761220f0588d696e08bf69ad7a47032815d
John Michael Brown <jmichaelbrown132737@gmail.com>
2019-02-22 10:13:42 -0800

    remove console logs, simplify key

--- src/components/EducationList.jsx
+++ src/components/EducationList.jsx
@@ -4,17 +4,14 @@ import PropTypes from 'prop-types';
 // import {} from '../css/styles.jsx';
 
 function EducationList(props){
-  console.log(props);
   return (
     <div>
       {Object.keys(props.educationList).map(function(educationId) {
         let education = props.educationList[educationId];
-        console.log(education);
-        console.log(educationId);
         return <Education
           institution={education.institution}
           skills={education.skillsGained}
-          key={`edu-${educationId}`}
+          key={educationId}
         />
       })}
     </div>
