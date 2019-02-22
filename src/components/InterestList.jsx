dee341b2d234f7df2107bae98e1806ca926fa96a
John Michael Brown <jmichaelbrown132737@gmail.com>
2019-02-22 10:14:06 -0800

    add refactor InterestList component

--- src/components/InterestList.jsx
+++ src/components/InterestList.jsx
@@ -1,29 +1,36 @@
 import React from 'react';
-import Portfolio from '../assets/portfolio.js';
 import Interest from './Interest';
+import PropTypes from 'prop-types';
 // import {} from '../css/styles.jsx';
 
-function InterestList(){
+function InterestList(props){
   return (
     <div>
       <h3>Technical Interests</h3>
-      {Portfolio.technicalInterestList.map((interest, index) =>
-        <Interest
-          regard={interest.regard}
-          notes={interest.notes}
-          key={`technical-interest-list-${index}`}
+      {Object.keys(props.technicalInterestList).map(function(interestId, index) {
+        let interest = props.technicalInterestList[interestId];
+        return <Interest
+        regard={interest.regard}
+        notes={interest.notes}
+        key={`${interestId}-${index}`}
         />
-      )}
+      })}
       <h3>Personal Interests</h3>
-      {Portfolio.personalInterestList.map((interest, index) =>
-        <Interest
-          regard={interest.regard}
-          notes={interest.notes}
-          key={`personal-interest-list-${index}`}
+      {Object.keys(props.personalInterestList).map(function(interestId, index) {
+        let interest = props.personalInterestList[interestId];
+        return <Interest
+        regard={interest.regard}
+        notes={interest.notes}
+        key={`${interestId}-${index}`}
         />
-      )}
+      })}
     </div>
   );
+}
+
+InterestList.propTypes = {
+  personalInterestList: PropTypes.object.isRequired,
+  technicalInterestList: PropTypes.object.isRequired
 }
 
 export default InterestList;
