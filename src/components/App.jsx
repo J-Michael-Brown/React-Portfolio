b492710dcff7246d61fa53eb92a6f01e9f4b2849
John Michael Brown <jmichaelbrown132737@gmail.com>
2019-02-22 08:31:59 -0800

    add necessary functionality to app component, will now pass info down to child components.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,22 +6,33 @@ import { Switch, Route } from 'react-router-dom';
 import EducationList from './EducationList';
 import ProjectList from './ProjectList';
 import InterestList from './InterestList';
+import Portfolio from '../assets/portfolio.js';
 /* eslint-enable */
 
-function App() {
+class App extends React.Component {
 
-  return (
+  constructor(props){
+    super(props);
+    this.state = {portfolio = Portfolio};
+  }
 
-    <div>
-      <Header/>
-      <switch>
-        <Route exact path='/education' component={EducationList}/>
-        <Route exact path='/projects' component={ProjectList}/>
-        <Route exact path='/interests' component={InterestList}/>
-      </switch>
-    </div>
+  render(){
+    return (
 
-  );
+      <div>
+        <Header/>
+        <switch>
+          <Route exact path='/education' render={()=><EducationList educationList={this.state.portfolio.educationList}/>}/>
+          <Route exact path='/projects' render={()=><ProjectList projectList={this.state.portfolio.projectList}/>}/>
+          <Route exact path='/interests' render={()=><InterestList
+             technicalInterestList={this.state.portfolio.technicalInterestList}
+             personalInterestList={this.state.portfolio.personalInterestList}/>}/>
+        </switch>
+      </div>
+
+    );
+  }
+
 }
 
 export default App;
