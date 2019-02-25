485a7d611e96ae56b878eb61e54de599a0490148
John Michael Brown <jmichaelbrown132737@gmail.com>
2019-02-24 19:49:05 -0800

    will better show in on page what tab user is on.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,17 +4,46 @@ import Navigation from 'react-toolbox/lib/navigation';
 import {Button} from 'react-toolbox/lib/button';
 import { Link } from 'react-router-dom';
 
-function Header(){
-  return (
+class Header extends React.Component{
 
-    <AppBar title="React Toolbox" leftIcon='' rightIcon=''>
-      <Navigation type="horizontal">
-        <Link to="/education"><Button label='Education' accent /></Link>
-        <Link to="/projects"><Button label='Projects' accent raised /></Link>
-        <Link to="/interests"><Button label='Interests' accent /></Link>
-      </Navigation>
-    </AppBar>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentRoute: ''
+    };
+    this.handleCurrentRoute = this.handleCurrentRoute.bind(this);
+  }
+
+  handleCurrentRoute(newRoute){
+    this.setState({currentRoute: newRoute});
+  }
+
+  render(){
+    let currentRoute = this.state.currentRoute;
+    let eduLink = <Link to="/education"><Button onClick={() => this.handleCurrentRoute('education')} label='Education' accent /></Link>;
+    let projectLink = <Link to="/projects"><Button onClick={() => this.handleCurrentRoute('projects')} label='Projects' accent /></Link>;
+    let interestLink = <Link to="/interests"><Button onClick={() => this.handleCurrentRoute('interests')} label='Interests' accent /></Link>;
+
+    if(currentRoute=='education'){
+      eduLink = <Link to="/education"><Button onClick={() => this.handleCurrentRoute('education')} label='Education' accent raised/></Link>
+    }
+    else if(currentRoute == 'projects'){
+      projectLink = <Link to="/projects"><Button onClick={() => this.handleCurrentRoute('projects')} label='Projects' accent raised /></Link>
+    }
+    else if(currentRoute == 'interests'){
+      interestLink = <Link to="/interests"><Button onClick={() => this.handleCurrentRoute('interests')} label='Interests' accent raised /></Link>
+    }
+
+    return (
+      <AppBar title="J. Michael Brown">
+        <Navigation type="horizontal">
+          {eduLink}
+          {projectLink}
+          {interestLink}
+        </Navigation>
+      </AppBar>
+    );
+  }
 }
 
 export default Header;
